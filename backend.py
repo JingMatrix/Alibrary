9d81ab7bfe0e85a7f88ebb95268f8c9f9c6b833a
JingMatrix <jingmatrix@gmail.com>
2022-11-14 02:22:31 +0100

    Use common api with GET and POST
    Costume methods for http is not supported by fecth()

--- backend.py
+++ backend.py
@@ -17,28 +17,25 @@ class SearchHanlder(BaseHTTPRequestHandler):
     index: Search = AliShareInfo.db().ft(index_name=':type.AliShareInfo:index')
     search_text: str = ''
 
-    def do_SEARCH(self):
-        type = self.headers.get('Content-Type')
-        if type is not None and type == 'application/x-www-form-urlencoded':
-            length = int(self.headers.get('Content-Length'))
-            self.search_text = unquote(self.rfile.read(length), encoding='utf-8')
-            self.send_response(200)
-            self.send_header('Content-Type', 'application/json')
-            self.end_headers()
-            self.wfile.write(self.retrieve())
+    def do_GET(self):
+        print(self.path)
+        self.search_text = unquote(self.path.split('?')[1])
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        self.wfile.write(self.retrieve())
 
-    def do_RELAY(self):
-        type = self.headers.get('Content-Type')
-        if type is not None and type == 'application/json':
-            length = int(self.headers.get('Content-Length'))
-            param = json.loads(self.rfile.read(length))
-            if 'file_id' in param:
-                hash = ':type.AliShareInfo:' + param['file_id']
-                if AliShareInfo.db().exists(hash):
-                    self.send_response(200)
-                    self.end_headers()
-                    share_id = AliShareInfo.db().hget(hash, 'share_id')
-                    self.wfile.write(Relay(file_id=param['file_id'], share_id=share_id).encode('utf-8'))
+    def do_POST(self):
+        length = int(self.headers.get('Content-Length'))
+        file_id = self.rfile.read(length).decode('utf-8')
+        hash = ':type.AliShareInfo:' + file_id
+        if AliShareInfo.db().exists(hash):
+            self.send_response(200)
+            self.send_header('Access-Control-Allow-Origin', '*')
+            self.end_headers()
+            share_id = AliShareInfo.db().hget(hash, 'share_id')
+            self.wfile.write(Relay(file_id=file_id, share_id=share_id).encode('utf-8'))
 
     def retrieve(self):
         if self.search_text is not None:
@@ -46,9 +43,8 @@ class SearchHanlder(BaseHTTPRequestHandler):
                 Query(self.search_text).language('chinese').paging(
                     self.offset, self.num))
             return json.dumps(
-                {'total': reuslts.total,
-                 'docs': [{'name': doc.name, 'size': doc.size, 'file_id': doc.file_id}
-                          for doc in reuslts.docs]}
+                [{'name': doc.name, 'size': doc.size, 'file_id': doc.file_id}
+                 for doc in reuslts.docs]
             ).encode('utf-8')
 
 
