88807d4aaad092f60d9f8a71ce423c5c9d5ff35b
JingMatrix <jingmatrix@gmail.com>
2022-11-19 11:51:59 +0100

    Fix SQL syntax
    Spaces are not allowed in tsquery

--- search.py
+++ search.py
@@ -1,5 +1,6 @@
 #! /bin/python3
 from psycopg2 import connect
+from psycopg2.errors import SyntaxError
 import os
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from urllib.parse import unquote
@@ -10,31 +11,44 @@ class SearchHanlder(BaseHTTPRequestHandler):
     offset: int = 0
     limit: int = 500
     total: int = 0
-    db = connect("port=5433 dbname=alibrary").cursor()
+    db = connect("dbname=alibrary").cursor()
     search_text: str = ''
+    sql: str = "SELECT id,name,size FROM record WHERE tsv @@ to_tsquery('jiebacfg', %s) LIMIT %s;"
     docs = []
+    result = '{"erorr": "Invalid query format"}'.encode()
 
     def do_GET(self):
-        self.send_response(200)
+        self.protocol_version = 'HTTP/1.1'
+        path = self.path.split('?')
+        if len(path) == 2:
+            self.search_text = unquote(self.path.split('?')[1]).strip()
+            self.result = self.retrieve()
+        else:
+            self.send_response(400)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Content-Type', 'application/json')
         self.end_headers()
-        path = self.path.split('?')
-        if len(path) == 2:
-            self.search_text = unquote(self.path.split('?')[1])
-            self.wfile.write(self.retrieve())
-        else:
-            self.wfile.write('{"erorr": "Invalid query format"}'.encode())
+        self.wfile.write(self.result)
 
     def retrieve(self):
         if self.search_text is not None:
             self.docs = []
-            self.db.execute("SELECT id,name,size FROM record WHERE tsv @@ to_tsquery('jiebacfg', %s) LIMIT %s;", (self.search_text, self.limit))
+            if ' ' in self.search_text and '&' not in self.search_text and '|' not in self.search_text and '<->' not in self.search_text and '!' not in self.search_text:
+                self.search_text = ' & '.join(self.search_text.split())
+            try:
+                self.db.execute(self.sql, (self.search_text, self.limit))
+            except SyntaxError as e:
+                self.send_response(400)
+                return json.dumps({"error": str(e).strip()}).encode()
+
             self.docs = self.db.fetchall()
-        return json.dumps(
-            [{'name': doc[1], 'size': int(doc[2]) * 1024, 'file_id': doc[0].split(':')[0], 'share_id': doc[0].split(':')[1]}
-             for doc in self.docs]
-        ).encode('utf-8')
+        self.send_response(200)
+        return json.dumps([{
+            'name': doc[1],
+            'size': int(doc[2]) * 1024,
+            'file_id': doc[0].split(':')[1],
+            'share_id': doc[0].split(':')[0]
+        } for doc in self.docs]).encode('utf-8')
 
 
 def run(server_class=HTTPServer, handler_class=SearchHanlder):
